31a94a2944cc1eb83b2ae8f926d55189ac3d63c6
Z_Z.W <myhongkongzhen@gmail.com>
2015-09-02 14:42:58 +0800

    增加httpPost(URL，msg) API

--- src/main/java/z/z/w/util/http/HttpClientUtil.java
+++ src/main/java/z/z/w/util/http/HttpClientUtil.java
@@ -1,41 +1,42 @@
-package z.z.w.util.http;
+package z.z.w.util.http ;
 
-import java.io.IOException;
-import java.io.InterruptedIOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.nio.charset.CodingErrorAction;
+import java.io.IOException ;
+import java.io.InterruptedIOException ;
+import java.net.MalformedURLException ;
+import java.net.URI ;
+import java.net.URISyntaxException ;
+import java.net.URL ;
+import java.net.UnknownHostException ;
+import java.nio.charset.CodingErrorAction ;
 
-import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLException ;
 
-import org.apache.http.Consts;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpEntityEnclosingRequest;
-import org.apache.http.HttpHost;
-import org.apache.http.HttpRequest;
-import org.apache.http.ParseException;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpRequestRetryHandler;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.config.ConnectionConfig;
-import org.apache.http.config.MessageConstraints;
-import org.apache.http.config.SocketConfig;
-import org.apache.http.conn.ConnectTimeoutException;
-import org.apache.http.conn.routing.HttpRoute;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.protocol.HttpContext;
-import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.http.Consts ;
+import org.apache.http.HttpEntity ;
+import org.apache.http.HttpEntityEnclosingRequest ;
+import org.apache.http.HttpHost ;
+import org.apache.http.HttpRequest ;
+import org.apache.http.ParseException ;
+import org.apache.http.client.ClientProtocolException ;
+import org.apache.http.client.HttpRequestRetryHandler ;
+import org.apache.http.client.config.RequestConfig ;
+import org.apache.http.client.methods.CloseableHttpResponse ;
+import org.apache.http.client.methods.HttpPost ;
+import org.apache.http.client.protocol.HttpClientContext ;
+import org.apache.http.config.ConnectionConfig ;
+import org.apache.http.config.MessageConstraints ;
+import org.apache.http.config.SocketConfig ;
+import org.apache.http.conn.ConnectTimeoutException ;
+import org.apache.http.conn.routing.HttpRoute ;
+import org.apache.http.entity.StringEntity ;
+import org.apache.http.impl.client.CloseableHttpClient ;
+import org.apache.http.impl.client.HttpClients ;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager ;
+import org.apache.http.protocol.HTTP ;
+import org.apache.http.protocol.HttpContext ;
+import org.apache.http.util.EntityUtils ;
+import org.slf4j.Logger ;
+import org.slf4j.LoggerFactory ;
 
 /**************************************************************************
  * <pre>
@@ -49,54 +50,54 @@ import org.slf4j.LoggerFactory;
  **************************************************************************/
 public class HttpClientUtil
 {
-	private static final Logger			logger					= LoggerFactory.getLogger( HttpClientUtil.class );
-	private static final int			MAX_TOTAL_CONNECTIONS	= 200;
-	private static final int			MAX_ROUTE_CONNECTIONS	= 50;
-	private static final HttpHost		DEFAULT_TARGETHOST		= new HttpHost( "http://localhost", 8888 );
-	private static final int			CONNECT_TIMEOUT			= 50000;
-	private static final int			SOCKET_TIMEOUT			= 50000;
-	private static final int			CONN_MANAGER_TIMEOUT	= 60000;
+	private static final Logger			logger					= LoggerFactory.getLogger( HttpClientUtil.class ) ;
+	private static final int			MAX_TOTAL_CONNECTIONS	= 200 ;
+	private static final int			MAX_ROUTE_CONNECTIONS	= 50 ;
+	private static final HttpHost		DEFAULT_TARGETHOST		= new HttpHost( "http://localhost", 8888 ) ;
+	private static final int			CONNECT_TIMEOUT			= 50000 ;
+	private static final int			SOCKET_TIMEOUT			= 50000 ;
+	private static final int			CONN_MANAGER_TIMEOUT	= 60000 ;
 	
-	private static final RequestConfig	config					= RequestConfig.custom().setSocketTimeout( SOCKET_TIMEOUT ).setConnectTimeout( CONNECT_TIMEOUT ).setConnectionRequestTimeout( CONN_MANAGER_TIMEOUT ).build();
+	private static final RequestConfig	config					= RequestConfig.custom().setSocketTimeout( SOCKET_TIMEOUT ).setConnectTimeout( CONNECT_TIMEOUT ).setConnectionRequestTimeout( CONN_MANAGER_TIMEOUT ).build() ;
 	
-	private static CloseableHttpClient	httpClient				= null;
+	private static CloseableHttpClient	httpClient				= null ;
 	
 	static
 	{
 		try
 		{
-			PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
-			SocketConfig socketConfig = SocketConfig.custom().setTcpNoDelay( true ).build();
-			connManager.setDefaultSocketConfig( socketConfig );
-			MessageConstraints messageConstraints = MessageConstraints.custom().setMaxHeaderCount( 200 ).setMaxLineLength( 2000 ).build();
-			ConnectionConfig connectionConfig = ConnectionConfig.custom().setMalformedInputAction( CodingErrorAction.IGNORE ).setUnmappableInputAction( CodingErrorAction.IGNORE ).setCharset( Consts.UTF_8 ).setMessageConstraints( messageConstraints ).build();
-			connManager.setDefaultConnectionConfig( connectionConfig );
-			connManager.setMaxTotal( MAX_TOTAL_CONNECTIONS );
-			connManager.setDefaultMaxPerRoute( MAX_ROUTE_CONNECTIONS );
-			connManager.setMaxPerRoute( new HttpRoute( DEFAULT_TARGETHOST ), 50 );
+			PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager() ;
+			SocketConfig socketConfig = SocketConfig.custom().setTcpNoDelay( true ).build() ;
+			connManager.setDefaultSocketConfig( socketConfig ) ;
+			MessageConstraints messageConstraints = MessageConstraints.custom().setMaxHeaderCount( 200 ).setMaxLineLength( 2000 ).build() ;
+			ConnectionConfig connectionConfig = ConnectionConfig.custom().setMalformedInputAction( CodingErrorAction.IGNORE ).setUnmappableInputAction( CodingErrorAction.IGNORE ).setCharset( Consts.UTF_8 ).setMessageConstraints( messageConstraints ).build() ;
+			connManager.setDefaultConnectionConfig( connectionConfig ) ;
+			connManager.setMaxTotal( MAX_TOTAL_CONNECTIONS ) ;
+			connManager.setDefaultMaxPerRoute( MAX_ROUTE_CONNECTIONS ) ;
+			connManager.setMaxPerRoute( new HttpRoute( DEFAULT_TARGETHOST ), 50 ) ;
 			
 			HttpRequestRetryHandler retryHandler = new HttpRequestRetryHandler()
 			{
 				public boolean retryRequest( IOException exception, int executionCount, HttpContext context )
 				{
-					if ( executionCount >= 5 ) return false;
-					if ( exception instanceof InterruptedIOException ) return false;
-					if ( exception instanceof UnknownHostException ) return false;
-					if ( exception instanceof ConnectTimeoutException ) return false;
-					if ( exception instanceof SSLException ) return false;
-					HttpClientContext clientContext = HttpClientContext.adapt( context );
-					HttpRequest request = clientContext.getRequest();
-					boolean idempotent = !( request instanceof HttpEntityEnclosingRequest );
-					if ( idempotent ) return true;
-					return false;
+					if ( executionCount >= 5 ) return false ;
+					if ( exception instanceof InterruptedIOException ) return false ;
+					if ( exception instanceof UnknownHostException ) return false ;
+					if ( exception instanceof ConnectTimeoutException ) return false ;
+					if ( exception instanceof SSLException ) return false ;
+					HttpClientContext clientContext = HttpClientContext.adapt( context ) ;
+					HttpRequest request = clientContext.getRequest() ;
+					boolean idempotent = !( request instanceof HttpEntityEnclosingRequest ) ;
+					if ( idempotent ) return true ;
+					return false ;
 				}
-			};
+			} ;
 			
-			httpClient = HttpClients.custom().setConnectionManager( connManager ).setRetryHandler( retryHandler ).build();
+			httpClient = HttpClients.custom().setConnectionManager( connManager ).setRetryHandler( retryHandler ).build() ;
 		}
 		catch ( Exception e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
 		}
 	}
 	
@@ -105,7 +106,29 @@ public class HttpClientUtil
 	 */
 	public static CloseableHttpClient getHttpClient()
 	{
-		return httpClient;
+		return httpClient ;
+	}
+	
+	/**
+	 * Create by : 2015年9月2日 下午2:37:29
+	 * 
+	 * @throws Exception
+	 */
+	public static String httpPost( String url, String msg ) throws Exception
+	{
+		try
+		{
+			StringEntity stringEntity = new StringEntity( msg, "utf-8" ) ;// 解决中文乱码问题
+			HttpPost httpPost = new HttpPost( url ) ;
+			httpPost.setEntity( stringEntity ) ;
+			
+			return doPost( httpPost ) ;
+		}
+		catch ( Exception e )
+		{
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
+		}
 	}
 	
 	/**
@@ -116,25 +139,25 @@ public class HttpClientUtil
 	{
 		try
 		{
-			URL url = new URL( httpUrl );
-			URI uri = new URI( url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null );
-			HttpPost httpPost = new HttpPost( uri );
-			return doPost( httpPost );
+			URL url = new URL( httpUrl ) ;
+			URI uri = new URI( url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), null ) ;
+			HttpPost httpPost = new HttpPost( uri ) ;
+			return doPost( httpPost ) ;
 		}
 		catch ( MalformedURLException e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 		catch ( URISyntaxException e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 		catch ( Exception e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 	}
 	
@@ -142,44 +165,44 @@ public class HttpClientUtil
 	{
 		try
 		{
-			httpPost.setConfig( config );
-			httpPost.addHeader( HTTP.CONTENT_TYPE, "application/json" );
-			httpPost.setHeader( "Accept-Charset", "gbk,GB2312,utf-8;q=0.7,*;q=0.7" );
-			httpPost.setHeader( "Accept-Language", "zh-cn,zh;q=0.5" );
-			httpPost.setHeader( HTTP.CONN_DIRECTIVE, "keep-alive" );
-			httpPost.setHeader( "refer", "sms.zhiyan.net" );
-			httpPost.setHeader( HTTP.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1; rv:6.0.2) Gecko/20100101 Firefox/6.0.2" );
+			httpPost.setConfig( config ) ;
+			httpPost.addHeader( HTTP.CONTENT_TYPE, "application/json" ) ;
+			httpPost.setHeader( "Accept-Charset", "gbk,GB2312,utf-8;q=0.7,*;q=0.7" ) ;
+			httpPost.setHeader( "Accept-Language", "zh-cn,zh;q=0.5" ) ;
+			httpPost.setHeader( HTTP.CONN_DIRECTIVE, "keep-alive" ) ;
+			httpPost.setHeader( "refer", "localhost" ) ;
+			httpPost.setHeader( HTTP.USER_AGENT, "Mozilla/5.0 (Windows NT 6.1; rv:6.0.2) Gecko/20100101 Firefox/6.0.2" ) ;
 			
-			CloseableHttpResponse response = httpClient.execute( httpPost, HttpClientContext.create() );
+			CloseableHttpResponse response = httpClient.execute( httpPost, HttpClientContext.create() ) ;
 			try
 			{
-				HttpEntity entity = response.getEntity();
-				return EntityUtils.toString( entity );
+				HttpEntity entity = response.getEntity() ;
+				return EntityUtils.toString( entity ) ;
 			}
 			finally
 			{
-				if ( null != response ) response.close();
+				if ( null != response ) response.close() ;
 			}
 		}
 		catch ( ClientProtocolException e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 		catch ( ParseException e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 		catch ( IOException e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 		catch ( Exception e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 	}
 	
@@ -187,20 +210,20 @@ public class HttpClientUtil
 	{
 		try
 		{
-			HttpPost httpPost = new HttpPost( url );
-			return doPost( httpPost );
+			HttpPost httpPost = new HttpPost( url ) ;
+			return doPost( httpPost ) ;
 		}
 		catch ( Exception e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 		
 	}
 	
 	private HttpClientUtil()
 	{
-		super();
+		super() ;
 	}
 	
 	/**
@@ -212,13 +235,13 @@ public class HttpClientUtil
 	{
 		try
 		{
-			HttpPost httpPost = new HttpPost( uri );
-			return doPost( httpPost );
+			HttpPost httpPost = new HttpPost( uri ) ;
+			return doPost( httpPost ) ;
 		}
 		catch ( Exception e )
 		{
-			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e );
-			throw e;
+			logger.error( "HttpClientUtil error : [{}].", e.getMessage(), e ) ;
+			throw e ;
 		}
 	}
 	
