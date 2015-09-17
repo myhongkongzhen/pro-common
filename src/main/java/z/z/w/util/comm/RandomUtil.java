9495419ca2616a0f908bf2a74159d55082b4ae33
Z_Z.W <myhongkongzhen@gmail.com>
2015-09-17 18:17:32 +0800

    add random test

---
+++ src/main/java/z/z/w/util/comm/RandomUtil.java
@@ -0,0 +1,151 @@
+package z.z.w.util.comm ;
+
+import java.util.Random ;
+
+/**************************************************************************
+ * <pre>
+ *     FileName: z.z.w.util.comm.RandomUtil.java
+ *         Desc: 
+ *      @author: Z_Z.W - myhongkongzhen@gmail.com
+ *     @version: 2015年9月17日 下午4:22:36 
+ *   LastChange: 2015年9月17日 下午4:22:36 
+ *      History:
+ * </pre>
+ **************************************************************************/
+public enum RandomUtil
+{
+	INSTANCE ;
+	public final String	ALLCHAR		= "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+	public final String	LETTERCHAR	= "abcdefghijkllmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" ;
+	public final String	NUMBERCHAR	= "0123456789" ;
+	
+	private RandomUtil()
+	{}
+	
+	/**
+	 * 返回一个定长的随机字符串(只包含大小写字母、数字)
+	 * 
+	 * @param length
+	 *            随机字符串长度
+	 * @return 随机字符串
+	 */
+	public String generateString( int length )
+	{
+		StringBuffer sb = new StringBuffer() ;
+		Random random = new Random() ;
+		for ( int i = 0 ; i < length ; i++ )
+		{
+			sb.append( ALLCHAR.charAt( random.nextInt( ALLCHAR.length() ) ) ) ;
+		}
+		return sb.toString() ;
+	}
+	
+	/**
+	 * 返回一个定长的随机纯字母字符串(只包含大小写字母)
+	 * 
+	 * @param length
+	 *            随机字符串长度
+	 * @return 随机字符串
+	 */
+	public String generateMixString( int length )
+	{
+		StringBuffer sb = new StringBuffer() ;
+		Random random = new Random() ;
+		for ( int i = 0 ; i < length ; i++ )
+		{
+			sb.append( ALLCHAR.charAt( random.nextInt( LETTERCHAR.length() ) ) ) ;
+		}
+		return sb.toString() ;
+	}
+	
+	/**
+	 * 返回一个定长的随机纯大写字母字符串(只包含大小写字母)
+	 * 
+	 * @param length
+	 *            随机字符串长度
+	 * @return 随机字符串
+	 */
+	public String generateLowerString( int length )
+	{
+		return generateMixString( length ).toLowerCase() ;
+	}
+	
+	/**
+	 * 返回一个定长的随机纯小写字母字符串(只包含大小写字母)
+	 * 
+	 * @param length
+	 *            随机字符串长度
+	 * @return 随机字符串
+	 */
+	public String generateUpperString( int length )
+	{
+		return generateMixString( length ).toUpperCase() ;
+	}
+	
+	/**
+	 * 生成一个定长的纯0字符串
+	 * 
+	 * @param length
+	 *            字符串长度
+	 * @return 纯0字符串
+	 */
+	public String generateZeroString( int length )
+	{
+		StringBuffer sb = new StringBuffer() ;
+		for ( int i = 0 ; i < length ; i++ )
+		{
+			sb.append( '0' ) ;
+		}
+		return sb.toString() ;
+	}
+	
+	/**
+	 * 根据数字生成一个定长的字符串，长度不够前面补0
+	 * 
+	 * @param num
+	 *            数字
+	 * @param fixdlenth
+	 *            字符串长度
+	 * @return 定长的字符串
+	 */
+	public String toFixdLengthString( long num, int fixdlenth )
+	{
+		StringBuffer sb = new StringBuffer() ;
+		String strNum = String.valueOf( num ) ;
+		if ( fixdlenth - strNum.length() >= 0 )
+		{
+			sb.append( generateZeroString( fixdlenth - strNum.length() ) ) ;
+		}
+		else
+		{
+			throw new RuntimeException( "将数字" + num + "转化为长度为" + fixdlenth + "的字符串发生异常！" ) ;
+		}
+		sb.append( strNum ) ;
+		return sb.toString() ;
+	}
+	
+	/**
+	 * 每次生成的len位数都不相同
+	 * 
+	 * @param param
+	 * @return 定长的数字
+	 */
+	public int getNotSimple( int[] param, int len )
+	{
+		Random rand = new Random() ;
+		for ( int i = param.length ; i > 1 ; i-- )
+		{
+			int index = rand.nextInt( i ) ;
+			int tmp = param[ index ] ;
+			param[ index ] = param[ i - 1 ] ;
+			param[ i - 1 ] = tmp ;
+		}
+		int result = 0 ;
+		for ( int i = 0 ; i < len ; i++ )
+		{
+			result = result * 10 + param[ i ] ;
+		}
+		return result ;
+	}
+	
+}
